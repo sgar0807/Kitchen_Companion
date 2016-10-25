a589b4e6964857c6427745c8a0743103e8884c09
Samuel Garcia <sgar0807@gmail.com>
2016-10-24 22:16:44 -0400

    Button Experimentation and Device Emulation Attempt (5 failed attempts)

--- app/src/main/java/www/gles/net/kitchen_companion/Main_Screen.java
+++ app/src/main/java/www/gles/net/kitchen_companion/Main_Screen.java
@@ -2,6 +2,8 @@ package www.gles.net.kitchen_companion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main_Screen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__screen);
+
+    }
+    public void buttonOnClick(View v) {
+        Button Recipes_Button =(Button) v;
+        ((Recipes_Button) v).setText("clicked");
     }
 }
